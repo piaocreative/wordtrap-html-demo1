a471c313fd911490ba6ad1829ef5096fccd48fbe
piaocreative <johnpiao171019@gmail.com>
2023-01-13 01:33:01 -0800

    Added sub pages

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 $( document ).ready( function() {
     
+    // Categories Slider
     if ( $( '.categories' ).length ) {
         $( '.categories' ).slick({
             infinite: true,
@@ -44,6 +45,7 @@ $( document ).ready( function() {
         });
     }  
 
+    // Logos Slider
     if ( $( '.logos' ).length ) {
         $( '.logos' ).slick({
             infinite: true,
@@ -79,5 +81,27 @@ $( document ).ready( function() {
             ]
         });
     }  
+
+    // Range Slider
+    if ( $( '[data-slider]' ).length ) {
+        $( '[data-slider]' ).slider({});
+
+        $( '#price_slider' ).on( 'slide', function( evt ) {
+            $( '#price_slider_value span' ).html( '$' + evt.value[0] + ' - ' + '$' + evt.value[1] );
+        });
+    }
+
+    // QTY
+    $('.quantity  #plus-btn').click(function(evt) {
+        evt.preventDefault();
+    	$('.qty').val(parseInt($('.qty').val()) + 1 );
+    });
+    $('.quantity #minus-btn').click(function(evt) {
+        evt.preventDefault();
+    	$('.qty').val(parseInt($('.qty').val()) - 1 );
+    	if ($('.qty').val() == 0) {
+			$('.qty').val(1);
+		}
+    });
 
 } );
