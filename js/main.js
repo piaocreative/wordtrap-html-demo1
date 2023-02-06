6eb8f282ab0de06a2993ea75b12c2af223384d6e
piaocreative <johnpiao171019@gmail.com>
2023-02-06 08:27:03 -0800

    Updated js

--- js/main.js
+++ js/main.js
@@ -50,7 +50,7 @@ $( document ).ready( function() {
 		$( '.logos' ).slick({
 			infinite: true,
 			slidesToShow: 5,
-			slidesToScroll: 5,
+			slidesToScroll: 1,
 			dots: false,
 			arrows: false,
 			responsive: [
@@ -59,7 +59,7 @@ $( document ).ready( function() {
 					settings: {
 						infinite: true,
 						slidesToShow: 4,
-						slidesToScroll: 4,
+						slidesToScroll: 1,
 					}
 				},
 				{
@@ -67,7 +67,7 @@ $( document ).ready( function() {
 					settings: {
 						infinite: true,
 						slidesToShow: 3,
-						slidesToScroll: 3,
+						slidesToScroll: 1,
 					}
 				},
 				{
@@ -75,7 +75,7 @@ $( document ).ready( function() {
 					settings: {
 						infinite: true,
 						slidesToShow: 2,
-						slidesToScroll: 2,
+						slidesToScroll: 1,
 					}
 				}
 			]
@@ -92,16 +92,25 @@ $( document ).ready( function() {
 	}
 
 	// QTY
-	$('.quantity  #plus-btn').click(function(evt) {
-		evt.preventDefault();
-		$('.qty').val(parseInt($('.qty').val()) + 1 );
-	});
-	$('.quantity #minus-btn').click(function(evt) {
-		evt.preventDefault();
-		$('.qty').val(parseInt($('.qty').val()) - 1 );
-		if ($('.qty').val() == 0) {
-			$('.qty').val(1);
-		}
-	});
+	if ( $('.quantity').length ) {
+		$('.quantity').each(function() {
+			var $this = $(this);
+			var $qty = $this.find('.qty');
+			var $plusBtn = $this.find('#plus-btn');
+			var $minusBtn = $this.find('#minus-btn');
+
+			$plusBtn.click(function(evt) {
+				evt.preventDefault();
+				$qty.val(parseInt($qty.val()) + 1 );
+			});
+			$minusBtn.click(function(evt) {
+				evt.preventDefault();
+				$qty.val(parseInt($qty.val()) - 1 );
+				if ($qty.val() == 0) {
+					$qty.val(1);
+				}
+			});
+		});
+	}
 
 } );
